3983f266d0f7d515ba7a56c7e2070f1895b2e0d9
Mark Jay Lunas <you@exagit config --global user.email markjay.lunas@gmail.com git config --global user.email you@exa>
2022-09-04 20:18:00 +0800

    added translayout file

---
+++ transitions/transLayout.ts
@@ -0,0 +1,11 @@
+export const motionLayout = {
+    initial: { opacity: 0, y: 20 },
+    final: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            type: 'spring',
+            duration: 0.8,
+        },
+    },
+};
