69f4a6f096cdb2f65f0d18e93f4da471d04773f4
Mark Jay Lunas <65273591+markjaylunas@users.noreply.github.com>
2022-09-22 22:01:43 +0800

    Remove question mark

--- components/Home/Updates.tsx
+++ components/Home/Updates.tsx
@@ -4,7 +4,7 @@ const Updates: React.FC = () => {
     return (
         <section className=" laptopSM:max-w-xl tablet:max-w-lg mobile:max-w-md   mobileMD:max-w-sm max-w-xs mx-auto ">
             <h2 className="font-text text-center text-3xl text-main-teal-light font-semibold mb-5">
-                What I&apos;ve been doing?
+                What I&apos;ve been doing
             </h2>
             {updates.map((update, key) => (
                 <p className="text-justify text-slate-600 indent-6" key={key}>
